aff648b99b73c7dddc7b71a62d3448153057d122
LIN88 <funterlin@gmail.com>
2020-12-07 22:17:17 +0800

    spec

--- clf.py
+++ clf.py
@@ -7,6 +7,27 @@ def png2vec(filename):
     img = mpimg.imread(filename)    
     return np.dot(img[...,:3], [0.299, 0.587, 0.114]).reshape(-1)
 
+def drawroc(y_test, P_test):
+    from sklearn.metrics import roc_curve, auc
+    # ROC Curve
+    fpr, tpr, threshold = roc_curve(y_test, P_test)
+    roc_auc = auc(fpr,tpr)
+    lw = 1
+    plt.figure()
+    #ax = plt.gca()
+    #ax.set_aspect(1)
+    plt.plot(fpr, tpr, color='darkorange',
+        lw=lw, label='ROC curve (area = %0.2f)' % roc_auc) ###假正率为横坐标，真正率为纵坐标做曲线
+    plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+    plt.xlim([-0.01, 1.0])
+    plt.ylim([0.0, 1.01])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+    plt.title('Receiver operating characteristic curve')
+    plt.legend(loc="lower right")
+    plt.show()
+    #plt.savefig('./roc_c=%0.1f.png'%Cval,dpi=300)
+    plt.clf()
     
 def RandomForest(n_tree, X_train, y_train, X_test, y_test):
     from sklearn.ensemble import RandomForestClassifier
@@ -20,43 +41,30 @@ def RandomForest(n_tree, X_train, y_train, X_test, y_test):
         print(clf.score(X_test, y_test))
         print()
 
+
 def rbfsvm(X_train, y_train, X_test, y_test):
     from sklearn.svm import SVC
-    from sklearn.metrics import roc_curve, auc
+    
     train_set = []
     test_set = []
     index = []
-    for Cval in range(1,3):
+    for Cval in range(2,3):
         index.append(Cval)
         trainx = X_train.copy()
         trainy = y_train.copy()
         clf = SVC(C = Cval, kernel = 'rbf', gamma= 'scale', class_weight = 'balanced')
         P_test = clf.fit(trainx,trainy).decision_function(X_test)
         print("C =",Cval)
-        #print(clf.n_support_)
+        print(clf.n_support_)
+        print(clf.score(X_train, y_train))
+        print(clf.score(X_test, y_test))
+        
         train_set.append(clf.score(X_train,y_train))
         test_set.append(clf.score(X_test, y_test))
+        drawroc(y_test, P_test)
         
         
-        # ROC Curve
-        fpr, tpr, threshold = roc_curve(y_test, P_test)
-        roc_auc = auc(fpr,tpr)
-        lw = 1
-        plt.figure()
-        #ax = plt.gca()
-        #ax.set_aspect(1)
-        plt.plot(fpr, tpr, color='darkorange',
-                lw=lw, label='ROC curve (area = %0.2f)' % roc_auc) ###假正率为横坐标，真正率为纵坐标做曲线
-        plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
-        plt.xlim([-0.01, 1.0])
-        plt.ylim([0.0, 1.01])
-        plt.xlabel('False Positive Rate')
-        plt.ylabel('True Positive Rate')
-        plt.title('Receiver operating characteristic curve')
-        plt.legend(loc="lower right")
         
-        plt.savefig('./roc_c=%0.1f.png'%Cval,dpi=300)
-        plt.clf()
         
     '''
     plt.figure(figsize = (10, 10))
@@ -77,17 +85,19 @@ def rbfsvm(X_train, y_train, X_test, y_test):
 
 def polysvm(X_train, y_train, X_test, y_test):
     from sklearn.svm import SVC
-    for Cval in range(1, 5):
-        for Deg in range(1, 5):
+    for Cval in range(1, 2):
+        for Deg in range(2, 3):
+            Cval = 1.8
             trainx = X_train.copy()
             trainy = y_train.copy()
             clf = SVC(C = Cval, kernel = 'poly', degree = Deg, gamma= 'scale', class_weight = 'balanced')
-            clf.fit(trainx,trainy)
-            print("C =",Cval)
+            P_test = clf.fit(trainx,trainy).decision_function(X_test)
+            print("C =", Cval, "degree:", Deg)
             print(clf.n_support_)
             print(clf.score(X_train,y_train))
             print(clf.score(X_test, y_test))
             print()
+            drawroc(y_test, P_test)
 
 def knnclf(X_train, y_train, X_test, y_test):
     from sklearn.neighbors import KNeighborsClassifier
@@ -101,38 +111,38 @@ def knnclf(X_train, y_train, X_test, y_test):
         print(clf.score(X_test, y_test))
         print()
 
+
 if __name__ == "__main__":
     '''
     vec = []
     lab = []
     for root, dirs, files in os.walk('./images'):
         for name in files:
-            if name.endswith('.png'):
+            if name.endswith('.png') and name[2] == 's':
                 vec.append(png2vec(root + '/' + name))
                 lab.append(int(name[0]))
         
     vec = np.array(vec)
-    lab = np.array(lab).reshape(len(lab), 1)
+    lab = np.array(lab)
     from sklearn.decomposition import PCA
     from sklearn.preprocessing import StandardScaler
     sl = StandardScaler()
     vec = sl.fit_transform(vec)
     print(vec.shape)
-    print(lab)
     print("PCA:")
     pca = PCA(n_components = 0.85)
     vec = pca.fit_transform(vec)
     print(pca.explained_variance_ratio_.shape)
     print(vec.shape)
-    fvec = open('./fvec.txt', 'wb')
-    flab = open('./flab.txt', 'wb')
+    fvec = open('./specvec.txt', 'wb')
+    flab = open('./speclab.txt', 'wb')
     pickle.dump(vec,fvec)
     pickle.dump(lab,flab)
     fvec.close()
     flab.close()
     '''
-    fvec = open('./fvec.txt','rb')
-    flab = open('./flab.txt','rb')
+    fvec = open('./specvec.txt','rb')
+    flab = open('./speclab.txt','rb')
     vec = pickle.load(fvec)
     lab = pickle.load(flab).ravel()
     fvec.close()
@@ -144,4 +154,6 @@ if __name__ == "__main__":
     from sklearn.model_selection import train_test_split
     X_train, X_test, y_train, y_test = train_test_split(vec, lab ,random_state=88)
     #rbfsvm(X_train, y_train, X_test, y_test)
-    print(X_train.shape)
+    #RandomForest(100, X_train, y_train, X_test, y_test)
+    polysvm(X_train, y_train, X_test, y_test)
+    
